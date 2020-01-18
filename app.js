276ecfe5896174bc55f4b8908f1149a2ae731f43
Daniel Ohn <ohndaniel@gmail.com>
2020-01-18 02:14:32 -0700

    Add walls to top and bottom.  Add controlls to player.

--- app.js
+++ app.js
@@ -28,17 +28,16 @@ function resize() {
 }
 
 let loader = PIXI.Loader.shared;
+let img;
 
 loader.add('bar', 'bar.png')
   .add('ball', 'ball.png')
+  .add ('wall', 'wall.png')
   .on('progress', handleLoaderProgress)
   .on('load', handleLoadAsset)
   .on('error', handleLoaderError)
   .load(setup);
 
-let img;
-
-let player, enemy, ball;
 
 function handleLoaderProgress(loader, resource) {
   console.log(loader.progress + " % loaded");
@@ -73,6 +72,19 @@ function animate() {
   img.rotation += 0.1;
 }
 
+let player, enemy, ball, wall;
+let walls = [];
+let floors = [];
+let up = keyboard('w');
+let down = keyboard('d');
+
+
+function makeWall() {
+  let wall_texture = loader.resources.wall.texture;
+  let new_wall = new PIXI.Sprite(wall_texture);
+  return new_wall;
+}
+
 function setup() {
   // Setting Resources and Main Container
   let stage = new PIXI.Container();
@@ -80,6 +92,7 @@ function setup() {
 
   let bar_texture = loader.resources.bar.texture;
   let ball_texture = loader.resources.ball.texture;
+
 
   // Setting up Sprites
   player = new PIXI.Sprite(bar_texture);
@@ -91,16 +104,57 @@ function setup() {
   ball = new PIXI.Sprite(ball_texture);
   stage.addChild(ball);
 
+  // wall = new PIXI.Sprite(wall_texture);
+  // stage.addChild(wall);
+
+  // wall.y = -200;
+
+  for (let i = 0; i < 75; i++) {
+    walls.push(makeWall());
+    stage.addChild(walls[i]);
+
+    walls[i].y = -200;
+    walls[i].x =  i * 8;
+
+    floors.push(makeWall());
+    stage.addChild(floors[i]);
+
+    floors[i].y = 200;
+    floors[i].x = i * 8;
+  }
+
   // Starting Positions 
   stage.x = app.screen.width / 2;
-  stage.y = app.screen.height / 2;
+  stage.y = app.screen.height;
 
   stage.pivot.x = stage.width / 2;
-  stage.pivot.y = stage.height / 2;
+  stage.pivot.y = stage.height;
 
-  player.x = -300;
-  enemy.x = 300;
+  player.x = 0;
+  enemy.x = 600;
+
+  player.vy = 0
+
+  player.anchor.set(.5);
+  enemy.anchor.set(.5);
+
   // Controls
+
+  up.press = () => {
+    player.vy = 1
+  };
+
+  up.release = () => {
+    player.vy  = 0;
+  };
+
+  down.press = () => {
+    player.vy = -1;
+  };
+
+  down.release = () => {
+    player.vy = 0;
+  };
 
   app.ticker.add(delta => game(delta));
 }
@@ -108,4 +162,50 @@ function setup() {
 function game(delta) {
   let speed = 3 * delta;
   ball.x += 1 * delta;
+  
+  player.y -= player.vy * speed;
+  
 }
+
+// keyboard stuff
+function keyboard(value) {
+  let key = {};
+  key.value = value;
+  key.isDown = false;
+  key.isUp = true;
+  key.press = undefined;
+  key.release = undefined;
+
+  key.downHandler = event => {
+    if (event.key === key.value) {
+      if (key.isUp && key.press) key.press();
+      key.isDown = true;
+      key.isUp = false;
+      event.preventDefault();
+    }
+  };
+
+  key.upHandler = event => {
+    if (event.key === key.value) {
+      if (key.isDown && key.release) key.release();
+      key.isDown = false;
+      key.isUp = true;
+      event.preventDefault();
+    }
+  };
+
+  // Attach Event listeners
+  const downListener = key.downHandler.bind(key);
+  const upListener = key.upHandler.bind(key);
+
+  window.addEventListener("keydown", downListener, false);
+  window.addEventListener("keyup", upListener, false);
+
+  // Detach event listeners
+  key.unsubscribe = () => {
+    window.removeEventListener("keydown", downListener);
+    window.removeEventListener("keyup", upListener);
+  };
+
+  return key;
+};
